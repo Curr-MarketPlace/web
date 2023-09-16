df2e0e7f2c34529942346c5750d58433d14e2bcd
melihkaan240 <melihkaanarac@gmail.com>
2023-09-16 18:08:57 +0300

    fixed: router

--- components/leftbar/Mainitems.tsx
+++ components/leftbar/Mainitems.tsx
@@ -1,40 +1,43 @@
 import Image from 'next/image'
 import React, { useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { FaReact } from 'react-icons/fa'
+
 import Link from 'next/link'
 
-const arr2 = [
-  { title: 'Home' },
-  { title: 'Market' },
-  { title: 'Trends' },
-  { title: 'Collections' }
-]
-
 const Bottombar = () => {
-  const router = useRouter()
-  const [url, setUrl] = useState('/')
   return (
     <div className='flex flex-col gap-4 items-start p-0 w-full h-auto '>
-      {arr2.map((i, index) => (
-        <div
-          key={index}
-          onClick={() => {
-            if (i.title === 'Home') {
-              setUrl('/')
-            } else {
-              setUrl(i.title)
-            }
-            router.push(`/${url}`)
-          }}
-          className='flex flex-row w-full justify-start gap-4 pl-8 cursor-pointer'
-        >
+      <Link href={`/`}>
+        <div className='flex flex-row w-full justify-start gap-4 pl-8 cursor-pointer'>
           {/* <FaReact width={20} height={20} style={style}  /> */}
           <Image src={'/vector.png'} alt='asd' width={20} height={20} />
 
-          <div className='textStyle '>{i.title}</div>
+          <div className='textStyle '>Home</div>
         </div>
-      ))}
+      </Link>
+      <Link href={`/Market`}>
+        <div className='flex flex-row w-full justify-start gap-4 pl-8 cursor-pointer'>
+          {/* <FaReact width={20} height={20} style={style}  /> */}
+          <Image src={'/vector.png'} alt='asd' width={20} height={20} />
+
+          <div className='textStyle '>Market</div>
+        </div>
+      </Link>
+      <Link href={`/Trends`}>
+        <div className='flex flex-row w-full justify-start gap-4 pl-8 cursor-pointer'>
+          {/* <FaReact width={20} height={20} style={style}  /> */}
+          <Image src={'/vector.png'} alt='asd' width={20} height={20} />
+
+          <div className='textStyle '>Trends</div>
+        </div>
+      </Link>
+      <Link href={`/Collections`}>
+        <div className='flex flex-row w-full justify-start gap-4 pl-8 cursor-pointer'>
+          {/* <FaReact width={20} height={20} style={style}  /> */}
+          <Image src={'/vector.png'} alt='asd' width={20} height={20} />
+
+          <div className='textStyle '>Collections</div>
+        </div>
+      </Link>
     </div>
   )
 }
--- components/leftbar/Profileitems.tsx
+++ components/leftbar/Profileitems.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React, { useState } from 'react'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 const arr = [
   { title: 'My Portfolio' },
@@ -15,25 +16,38 @@ const Topbar = () => {
 
   return (
     <div className='flex flex-col  gap-4 items-start p-0 w-full h-auto'>
-      {arr.map((i, index) => (
-        <div
-          key={index}
-          onClick={() => {
-            if (i.title === 'My Portfolio') {
-              setUrl('Profile')
-            } else {
-              setUrl(i.title)
-            }
-            router.push(`/${url}`)
-          }}
-          className='flex flex-row  w-full justify-start gap-4 pl-8 cursor-pointer'
-        >
+      <Link href={'/Profile'}>
+        <div className='flex flex-row  w-full justify-start gap-4 pl-8 cursor-pointer'>
           <Image src={'/vector.png'} alt='asd' width={20} height={20} />
           <div className='text-[#616161] text-lg font-semibold md:textStyle '>
-            {i.title}
+            My Portfolio
           </div>
         </div>
-      ))}
+      </Link>
+      <Link href={'/Favourites'}>
+        <div className='flex flex-row  w-full justify-start gap-4 pl-8 cursor-pointer'>
+          <Image src={'/vector.png'} alt='asd' width={20} height={20} />
+          <div className='text-[#616161] text-lg font-semibold md:textStyle '>
+            Favourites
+          </div>
+        </div>
+      </Link>
+      <Link href={'/History'}>
+        <div className='flex flex-row  w-full justify-start gap-4 pl-8 cursor-pointer'>
+          <Image src={'/vector.png'} alt='asd' width={20} height={20} />
+          <div className='text-[#616161] text-lg font-semibold md:textStyle '>
+            History
+          </div>
+        </div>
+      </Link>
+      <Link href={'/Settings'}>
+        <div className='flex flex-row  w-full justify-start gap-4 pl-8 cursor-pointer'>
+          <Image src={'/vector.png'} alt='asd' width={20} height={20} />
+          <div className='text-[#616161] text-lg font-semibold md:textStyle '>
+            Settings
+          </div>
+        </div>
+      </Link>
     </div>
   )
 }
